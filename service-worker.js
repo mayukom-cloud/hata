5fbad08facce65f05b4fcd5d526ec1b024cec46a
mayukom-cloud <mayuko.m@handsomegarden.com>
2026-07-07 08:03:24 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,15 @@
-const CACHE_NAME = 'hatanote-v1';
+const CACHE_NAME = 'hatanote-v2';
+const FIREBASE_VERSION = '12.15.0';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
-  './icon-512-maskable.png'
+  './icon-512-maskable.png',
+  `https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-app.js`,
+  `https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-firestore.js`,
+  `https://www.gstatic.com/firebasejs/${FIREBASE_VERSION}/firebase-auth.js`
 ];
 
 self.addEventListener('install', (event) => {
@@ -31,7 +35,9 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request).then((cached) => {
       const network = fetch(event.request)
         .then((response) => {
-          if (response && response.status === 200 && event.request.url.startsWith(self.location.origin)) {
+          const isSameOrigin = event.request.url.startsWith(self.location.origin);
+          const isFirebaseSdk = event.request.url.startsWith('https://www.gstatic.com/firebasejs/');
+          if (response && response.status === 200 && (isSameOrigin || isFirebaseSdk)) {
             const copy = response.clone();
             caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
           }
